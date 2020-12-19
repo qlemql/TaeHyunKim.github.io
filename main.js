8c672d319ecb3b35d35548f96b39c50a2c9cacea
qlemql <busgamer@naver.com>
2020-12-19 10:32:36 +0900

    JS__PROJECT__FILTERING

--- main.js
+++ main.js
@@ -75,15 +75,17 @@ workBtnContainer.addEventListener("click", (e)=>{
     if(filter == null){
         return;
     }
-
-    console.log(filter);
-    projects.forEach((project) =>{
-        if(filter === "*" || filter === project.dataset.type){
-            project.classList.remove("invisible");
-        } else{
-            project.classList.add("invisible");
-        }
-    });
+    projectContainer.classList.add('anim-out');
+    setTimeout(()=>{
+        projects.forEach((project) =>{
+            if(filter === "*" || filter === project.dataset.type){
+                project.classList.remove("invisible");
+            } else{
+                project.classList.add("invisible");
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    },300);
     
 
 });
